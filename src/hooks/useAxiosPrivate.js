a8962b593442cf15b30001c79090fcba511ee59b
Ali Hatami <a.hatam008@gmail.com>
2022-10-16 21:48:16 +0330

    axios private bug fixed

--- src/hooks/useAxiosPrivate.js
+++ src/hooks/useAxiosPrivate.js
@@ -11,7 +11,7 @@ const useAxiosPrivate = () => {
     const {auth} = useAuth();
 
     useEffect(() => {
-        const requestIntercept = axiosPrivate.request.interceptors.use(
+        const requestIntercept = axiosPrivate.interceptors.request.use(
             config => {
                 if (!config.headers['Authorization']) {
                     config.headers['Authorization'] = `Bearer ${auth?.token}`
