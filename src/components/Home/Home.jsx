64b744896bb24a412d0fe4046366d01637b241d8
Ali Hatami <a.hatam008@gmail.com>
2022-10-17 12:45:25 +0330

    navigate speed dial added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -7,15 +7,20 @@ import React from 'react'
 import { Outlet } from 'react-router-dom'
 import { redirects } from '../../api/urls'
 import Polls from '../Polls/Polls'
+import NavigateSpeedDial from '../SpeedDial/NavigateSpeedDial';
+
 
 import './Home.css'
 
 function Home() {
   return (
+    <>
+    <NavigateSpeedDial />
     <div className="Home">
       <Polls redirectTo={redirects.poll_page}/>
       <Outlet />
     </div>
+    </>
   )
 }
 
