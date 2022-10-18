0beed5ff72a7868a8dec4dda44b3e40953e0bba4
Ali Hatami <a.hatam008@gmail.com>
2022-10-18 14:30:19 +0330

    navigation dial links added
    + logout
    + home

--- src/components/SpeedDial/NavigateSpeedDial.jsx
+++ src/components/SpeedDial/NavigateSpeedDial.jsx
@@ -5,33 +5,45 @@
 
 import * as React from 'react';
 import SpeedDial from '@mui/material/SpeedDial';
-import SpeedDialIcon from '@mui/material/SpeedDialIcon';
 import SpeedDialAction from '@mui/material/SpeedDialAction';
-import FileCopyIcon from '@mui/icons-material/FileCopyOutlined';
-import SaveIcon from '@mui/icons-material/Save';
-import PrintIcon from '@mui/icons-material/Print';
-import ShareIcon from '@mui/icons-material/Share';
-
-const actions = [
-  { icon: <FileCopyIcon />, name: 'Copy' },
-  { icon: <SaveIcon />, name: 'Save' },
-  { icon: <PrintIcon />, name: 'Print' },
-  { icon: <ShareIcon />, name: 'Share' },
-];
+import SettingsIcon from '@mui/icons-material/Settings';
+import LogoutIcon from '@mui/icons-material/Logout';
+import HomeIcon from '@mui/icons-material/Home';
+import useAuth from '../../hooks/useAuth';
+import { useNavigate } from 'react-router-dom';
 
 export default function NavigateSpeedDial() {
+  const navigate = useNavigate();
+  const { setAuth } = useAuth();
+
+
+  const logout = () => {
+    setAuth({});
+    navigate('/login');
+  }
+
+  const goHome = () => {
+    navigate('/', {replace: true})
+  }
+
+  const actions = [
+    { icon: <LogoutIcon />, name: 'Logout', action: logout },
+    { icon: <HomeIcon />, name: 'Go To Home', action: goHome }
+  ];
+
+
   return (
     <SpeedDial
       ariaLabel="Actions"
       sx={{ position: 'absolute', bottom: 16, right: 16 }}
-      icon={<SpeedDialIcon />}
+      icon={<SettingsIcon />}
     >
       {actions.map((action) => (
         <SpeedDialAction
           key={action.name}
           icon={action.icon}
           tooltipTitle={action.name}
-          
+          onClick={action.action}
         />
       ))}
     </SpeedDial>
